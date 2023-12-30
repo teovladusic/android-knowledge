53bb27e834344d84657702ccddc0fa3c1c73f957
teovladusic <teo.vladusic@gmail.com>
2023-12-30 10:36:47 +0100

    revert status bar from revolut-clone

--- app/src/main/java/com/puzzle_agency/androidknowledge/ui/MainActivity.kt
+++ app/src/main/java/com/puzzle_agency/androidknowledge/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.puzzle_agency.androidknowledge.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,7 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
--- app/src/main/java/com/puzzle_agency/androidknowledge/ui/theme/Theme.kt
+++ app/src/main/java/com/puzzle_agency/androidknowledge/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.puzzle_agency.androidknowledge.ui.theme
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -58,8 +58,8 @@ fun AndroidKnowledgeTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.TRANSPARENT
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
